77a58cd5c67a6948a3bb9c57467809e88cd04e23
Sosolidkk <pedrochaveslimas3@gmail.com>
2020-03-28 21:56:38 -0300

    <feat>(src/index):Added sign_up form html

--- src/index/routes.py
+++ src/index/routes.py
@@ -1,5 +1,5 @@
 from . import index_blueprint
-from .forms import SignInForm
+from .forms import SignInForm, SignUpForm
 from flask import (
     flash,
     jsonify,
@@ -23,5 +23,18 @@ def index():
             flash("Logged in successful", "success")
             return redirect(url_for("index.index"))
 
-        flash("Error! Incorrect login credentials", "danger")
-    return render_template("index.html", form=form)
+        flash("Error on sign in", "danger")
+    return render_template("index.html", title="Index", brand="!", form=form)
+
+
+@index_blueprint.route("/sign_up", methods=["GET", "POST"])
+def sign_up():
+    form = SignUpForm(request.form)
+
+    if request.method == "POST":
+        if form.validate_on_submit():
+            flash("Sign up successful", "success")
+            return redirect(url_for("index.index"))
+
+        flash("Error on sign up", "danger")
+    return render_template("sign_up.html", title="Sign up", brand="!", form=form)
